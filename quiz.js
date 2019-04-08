7686ff9c638a1ccbf947ee5d354645a3b189b544
mattrmaydew <49329050+mattrmaydew@users.noreply.github.com>
2019-04-07 19:36:09 -0700

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,4 +1,6 @@
 <script src="https://github.com/browserify/browserify/blob/master/index.js"></script>
+<script type="text/javascript" async="" src="https://ssl.google-analytics.com/ga.js"></script>
+<script type="text/javascript" src="https://static.runkitcdn.com/assets/main-sha512-HLNUd__G_oQxtqlAlxxFeRIMp8Rtr5V0u846PBptJWGQa1jNgXsQpCMt6F6Uclz3NFpLXtJvIlFEdMn9ahvn2w==.bundle.js" integrity="sha512-HLNUd//G/oQxtqlAlxxFeRIMp8Rtr5V0u846PBptJWGQa1jNgXsQpCMt6F6Uclz3NFpLXtJvIlFEdMn9ahvn2w==" crossorigin="anonymous"></script>
 <script src="fernetBrowser.js"></script>
 let fernet = require('./fernet');
 let secret = new fernet.Secret('TluxwB3fV_GWuLkR1_BzGs1Zk90TYAuhNMZP_0q4WyM=')
