ac22ca895cd8a85461ac2e65423bc8b784327be9
mattrmaydew <49329050+mattrmaydew@users.noreply.github.com>
2019-04-07 19:26:54 -0700

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,1 +1,10 @@
-
+<script src="https://github.com/browserify/browserify/blob/master/index.js"></script>
+<script src="fernetBrowser.js"></script>
+let fernet = require('./fernet');
+let secret = new fernet.Secret('TluxwB3fV_GWuLkR1_BzGs1Zk90TYAuhNMZP_0q4WyM=')
+var token = new fernet.Token({
+  secret: secret,
+  token: 'gAAAAABcqVVf_7pKfMfb0vgBQx3L8wxNNh2Zv1rQ652aJ7cQzeooD7H4r5DmVBmQ4Vp_0OyUYiRBVjrnPg7jc9ktWNf33DjWz4agQjEV_djlQ0heoeiKcM7HgDmvwIDSpMxfL9fVh-PsJdf7DH_ITDr0h3rTemQncg4DGrmaWeWEiRaii8RNYwv0IzHuyTVC6fKNOhBDpw8n==',
+  ttl: 0
+})
+token.decode();
