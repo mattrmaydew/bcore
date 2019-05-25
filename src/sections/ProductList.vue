9dff1efd9c050610cbff4aa5d87fd36ebb1927b6
mattrmaydew <49329050+mattrmaydew@users.noreply.github.com>
2019-05-24 22:18:49 -0500

    Add files via upload

---
+++ src/sections/ProductList.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="products" id="products">
+    <div class="container">
+      <h1 class="text-center p-5">Our Products List</h1>
+      <div class="row">
+        <div class="col-md-4" v-for="(product,index) in products" :key="index" >
+          <div class="card product-item" >
+            <carousel :perPage="1">
+              <slide v-for="(image, index) in product.images" :key="index">
+                <img :src="image" alt="..." height="250px" >
+              </slide>
+            </carousel>
+
+            <div class="card-body">
+              <div class="d-flex justify-content-between">
+                <h5 class="card-title">{{product.name}}</h5>
+                <h5 class="card-prices">{{product.price | currency}}</h5>
+              </div>
+              <add-to-cart
+                :product-image="getImage(product.images)"
+                :name="product.name"
+                :price="product.price"
+                :productId="product.id"
+              ></add-to-cart>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { db } from "../firebase";
+
+export default {
+  name: "Products-list",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      products: []
+    };
+  },
+
+  methods: {
+    getImage(images) {
+      return products[0];
+      
+    },
+    
+  },
+
+  firestore() {
+    return {
+      products: db.collection("products")
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.products {
+  margin-top: 7rem;
+  background: #f2f2f2;
+  padding-bottom: 3rem;
+}
+</style>
